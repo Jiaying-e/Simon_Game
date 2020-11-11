25230d7a1a872526aee5ea9c0271a9188ecf8615
Jiaying-e <68321745+Jiaying-e@users.noreply.github.com>
2020-11-11 20:37:34 +1000

    Add files via upload

--- game.js
+++ game.js
@@ -1,9 +1,22 @@
 // alert("working");
 var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-function nextSequence(){
-  var randomNumber = Math.floor(Math.random()*4);
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-}
-nextSequence();
+
+  $("#" + randomChosenColour).on("click", function() {
+        $("." + randomChosenColour).fadeOut(100).fadeIn(100);
+        makesound(randomChosenColour);
+
+
+
+        function makesound(key) {
+
+          $("#" + key).click(function() {
+
+            var sound = new Audio("sounds/" + key + ".mp3");
+
+            return sound.play();})}; $(".btn").click(nextSequence);
