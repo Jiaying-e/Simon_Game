1757004bc2012550bb8864ff928e1a6b9d2c988a
Jiaying-e <68321745+Jiaying-e@users.noreply.github.com>
2020-11-17 15:32:46 +1000

    Add files via upload
    Check which button is pressed

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
-// alert("working");
-var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePattern = [];
+
+var userClickedPattern = [];
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+})
 function nextSequence() {
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).on("click", function() {
-        $("." + randomChosenColour).fadeOut(100).fadeIn(100);
-        makesound(randomChosenColour);
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
+}
 
-
-        function makesound(key) {
-
-          $("#" + key).click(function() {
-
-            var sound = new Audio("sounds/" + key + ".mp3");
-
-            return sound.play();})}; $(".btn").click(nextSequence);
+$("body").click(function() {
+  nextSequence()
+});
