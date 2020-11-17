5281019fec5203fc038388c1fa7e5dbd0c22aca0
Jiaying-e <68321745+Jiaying-e@users.noreply.github.com>
2020-11-17 15:48:28 +1000

    Add files via upload
    Add sound to button clicks

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ $(".btn").click(function(){
   var userChosenColour = $(this).attr("id");
 
   userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
 })
 function nextSequence() {
 
@@ -18,9 +19,13 @@ function nextSequence() {
   //1. Use jQuery to select the button with the same id as the randomChosenColour
   //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 
-  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+
+}
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
